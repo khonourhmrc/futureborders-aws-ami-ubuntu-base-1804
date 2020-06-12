c151badd8e3f981e043cef5d719648afe29de4c4
krystan honour <krystan.a.honour@capgemini.com>
2020-06-12 14:21:28 +0100

    moved to chrony

--- test/integration/inspec/12_chrony_spec.rb
+++ test/integration/inspec/12_chrony_spec.rb
@@ -1,8 +1,8 @@
-describe service('systemd-timesyncd') do
+describe service('chronyd') do
   it { should be_enabled }
   it { should be_running }
 end
 
-describe file('/etc/systemd/timesyncd.conf') do
+describe file('/etc/chrony/chrony.conf') do
   its('content') { should include('169.254.169.123') }
 end
