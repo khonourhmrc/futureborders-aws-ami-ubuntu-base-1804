3d987e2dc4bd74e507e98a2db69b1378a8974e65
krystan honour <krystan.a.honour@capgemini.com>
2020-06-12 12:56:30 +0100

    removed customer target

--- test/scripts/bootstrap.sh
+++ test/scripts/bootstrap.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 counter=0
-while ! systemctl status ready.target --no-pager > /dev/null; do
+# while ! systemctl status ready.target --no-pager > /dev/null; do
+while ! systemctl is-active --quiet multi-user.target --no-pager; do
   echo "Waiting for box to become ready... ${counter}s"
   sleep 10
   counter=$((counter+10))
