a86e335b727647fa20150afc5d82f4839b6415ae
krystan honour <krystan.a.honour@capgemini.com>
2020-06-12 13:31:08 +0100

    removed iam_sync tests

--- test/integration/inspec/10_install_iam_sync_spec.rb
+++ test/integration/inspec/10_install_iam_sync_spec.rb
@@ -1,14 +1,14 @@
-%w(
-  /usr/local/sbin/authorized_keys_command.sh
-  /usr/local/sbin/iam_sync.sh
-).each do |filename|
-  describe file (filename) do
-    it { should exist }
-    its(:owner) { should eq('root') }
-    its(:group) { should eq('root') }
-    its(:mode) { should cmp('0755') }
-  end
-end
+# %w(
+#   /usr/local/sbin/authorized_keys_command.sh
+#   /usr/local/sbin/iam_sync.sh
+# ).each do |filename|
+#   describe file (filename) do
+#     it { should exist }
+#     its(:owner) { should eq('root') }
+#     its(:group) { should eq('root') }
+#     its(:mode) { should cmp('0755') }
+#   end
+# end
 
 # We shouldn't have any non-system users
 control 'Users:' do
